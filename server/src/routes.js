c5b48698291b09b1a513155f0c4206dbfb3e49b4
dragon123456789123 <ismail.mounjid@gmail.com>
2019-02-13 17:56:15 +0100

    initiap app set up

---
+++ server/src/routes.js
@@ -0,0 +1,15 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+/* const SongsController = require('./controllers/SongsController')
+const BookmarksController = require('./controllers/BookmarksController')
+const HistoriesController = require('./controllers/HistoriesController')
+
+const isAuthenticated = require('./policies/isAuthenticated')*/
+
+module.exports = (app) => {
+  app.post('/register',
+    AuthenticationControllerPolicy.register,
+    AuthenticationController.register),
+  app.post('/login',
+    AuthenticationController.login)
+}
